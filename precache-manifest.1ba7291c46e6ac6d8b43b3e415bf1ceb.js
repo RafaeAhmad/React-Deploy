62c9be3ec794d47824a1b9a124e53d0528e13c18
RafaeAhmad <imrafae@gmail.com>
2020-08-15 23:27:57 -0500

    Updates

---
+++ precache-manifest.1ba7291c46e6ac6d8b43b3e415bf1ceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a80015542cfb5a0cb85428ed304fee",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "afe623e0487d9f26679a",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/React-Deploy/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Deploy/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe623e0487d9f26679a",
+    "url": "/React-Deploy/static/js/main.d53df74e.chunk.js"
+  },
+  {
+    "revision": "15600bf6b1addcd1b4a1",
+    "url": "/React-Deploy/static/js/runtime-main.f8ed63a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
